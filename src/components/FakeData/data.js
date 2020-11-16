6d8a5621b2561630fa4e491bc7f6118d6e059d86
aifa21 <37891597+aifa21@users.noreply.github.com>
2020-11-16 22:53:45 +0600

    added

---
+++ src/components/FakeData/data.js
@@ -0,0 +1,41 @@
+const data=[
+    {
+        "id":"1",
+        "name":'Psycho-training',
+        "img":"https://i.ibb.co/g9km3jM/psycho-training.jpg" 
+        
+    },
+    {
+        "id":"2",
+        "name":'Self-defense',
+        "img":"https://i.ibb.co/wLSBpKb/self-defense.jpg" 
+    },
+    {
+        "id":"3",
+        
+        "name":'Advanced gym',
+        "img":"https://i.ibb.co/7pvV4kF/advanced-gym.jpg"
+    },
+    {
+        "id":"4",
+        
+        "name":'Cardio training',
+        "img":"https://i.ibb.co/LSmCRn9/cardio-training.jpg" 
+    },
+    {
+        "id":"5",
+      
+        "name":'Strength training',
+        "img":"https://i.ibb.co/w6zXwCV/strength-training.jpg" 
+    },
+    {
+        "id":"6",
+        
+        "name":'Psycho training',
+        "img":"https://i.ibb.co/ByChyBP/psycho-training1.jpg"
+    }
+    
+    
+
+]
+export default data;
